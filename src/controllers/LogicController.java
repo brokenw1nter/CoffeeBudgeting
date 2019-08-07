542c08c54805c1627022084c1e177656f2f03eee
brokenw1nter <Timothy1403@gmail.com>
2019-08-07 01:27:09 -0600

    Added a LOT
    Added 2 new scenes, added quite a lot of methods (some are empty as still testing at the moment), and added a lot of new labels for the main scene.

--- src/controllers/LogicController.java
+++ src/controllers/LogicController.java
@@ -1,7 +1,85 @@
 package controllers;
 
+import java.util.Calendar;
+import java.text.NumberFormat;
+import java.text.DateFormatSymbols;
+
 public class LogicController {
 	
+	private static int page;
+	private static int year;
+	private static int monthNumber;
+	private static String monthName;
+	private static String monthYearLabel;
+	private static double money;
+	private static double totalFunds;
+	private static double totalIncome;
+	private static double totalExpenses;
+	private static NumberFormat fmt = NumberFormat.getCurrencyInstance();
 	
+	// --------- Methods for Buttons ----------
+	public static String previousMonth() {
+		if(monthNumber == 1) {
+			monthNumber = 12;
+			monthName = getMonthName(monthNumber);
+			year--;
+			monthYearLabel = monthName + " " + year;
+		}  else {
+			monthNumber --;
+			monthName = getMonthName(monthNumber);
+			monthYearLabel = monthName + " " + year;
+		}
+		return monthYearLabel;
+	}
+	
+	public static String nextMonth() {
+		if(monthNumber == 12) {
+			monthNumber = 1;
+			monthName = getMonthName(monthNumber);
+			year++;
+			monthYearLabel = monthName + " " + year;
+		} else {
+			monthNumber ++;
+			monthName = getMonthName(monthNumber);
+			monthYearLabel = monthName + " " + year;
+		}
+		return monthYearLabel;
+	}
+	
+	public static void addTransaction() {
+		System.out.println("Add Transaction is Working");
+	}
+	
+	// ------------ Helper Methods ------------
+	public static String getCurrentMonthYear() {
+		Calendar cal = Calendar.getInstance();
+		monthNumber = cal.get(Calendar.MONTH) + 1;
+		monthName = getMonthName(monthNumber);
+		year = cal.get(Calendar.YEAR);
+		monthYearLabel = monthName + " " + year;
+		return monthYearLabel;
+	}
+	
+	public static String getMonthName(int monthNumber) {
+		return new DateFormatSymbols().getMonths()[monthNumber - 1];
+	}
+	
+	public static String getIncomeTotal(int amount) {
+		totalIncome += amount;
+		String incomeTotalLabel = fmt.format(totalIncome);
+		return incomeTotalLabel;
+	}
+	
+	public static String getExpensesTotal(int amount) {
+		totalExpenses += amount;
+		String expensesTotalLabel = fmt.format(totalExpenses);
+		return expensesTotalLabel;
+	}
+	
+	public static String getMoneyTotal(int amount) {
+		totalFunds += amount;
+		String totalLabel = fmt.format(totalFunds);
+		return totalLabel;
+	}
 	
 }
--- src/controllers/SceneController.java
+++ src/controllers/SceneController.java
@@ -1,15 +1,104 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 import javafx.application.Platform;
+import javafx.scene.control.MenuBar;
+import javafx.scene.input.InputEvent;
 
 public class SceneController {
 	
+	private static String previousScene = "Main";
+	
+	@FXML private MenuBar menuBar = new MenuBar();
+	@FXML private Label monthYearLabel = new Label();
+	@FXML private Label incomeLabel = new Label();
+	@FXML private Label expensesLabel = new Label();
+	@FXML private Label totalLabel = new Label();
+	@FXML private Label firstDay = new Label();
+	@FXML private Label firstDayName = new Label();
+	@FXML private Label firstLabel = new Label();
+	@FXML private Label secondDay = new Label();
+	@FXML private Label secondDayName = new Label();
+	@FXML private Label secondLabel = new Label();
+	@FXML private Label thirdDay = new Label();
+	@FXML private Label thirdDayName = new Label();
+	@FXML private Label thirdLabel = new Label();
+	@FXML private Label fourthDay = new Label();
+	@FXML private Label fourthDayName = new Label();
+	@FXML private Label fourthLabel = new Label();
+	@FXML private Label fifthDay = new Label();
+	@FXML private Label fifthDayName = new Label();
+	@FXML private Label fifthLabel = new Label();
+	@FXML private Label pageLabel = new Label();
+	
+	// --------- Methods for Buttons ----------
+	@FXML public void previousMonth(InputEvent event) {
+		monthYearLabel.setText(LogicController.previousMonth());
+	}
+	
+	@FXML public void nextMonth(InputEvent event) {
+		monthYearLabel.setText(LogicController.nextMonth());
+	}
+	
+	@FXML public void newTransaction(InputEvent event) {
+		changeScene("/fxmls/NewTransaction.fxml");
+	}
+	
+	@FXML public void cancelTransaction(InputEvent event) {
+		clearTextFields();
+		changeScene("/fxmls/Main.fxml");
+	}
+	
+	@FXML public void confirmTransaction(InputEvent event) {
+		LogicController.addTransaction();
+		clearTextFields();
+		changeScene("/fxmls/Main.fxml");
+	}
+	
 	// ------ Methods for Menu Bar Items ------
-	@FXML
-	public void quitApplication(ActionEvent event) {
+	@FXML public void returnScene(ActionEvent event) {
+		if(previousScene == "Main") {
+			changeScene("/fxmls/Main.fxml");
+		}
+	}
+	
+	@FXML public void aboutApplication(ActionEvent event) {
+		changeScene("/fxmls/About.fxml");
+	}
+	
+	@FXML public void quitApplication(ActionEvent event) {
 		Platform.exit();
+	}
+	
+	// ------------ Helper Methods ------------
+	public void initialize() {
+		monthYearLabel.setText(LogicController.getCurrentMonthYear());
+		incomeLabel.setTextFill(Color.BLUE);
+		incomeLabel.setText("$25.00");
+		expensesLabel.setTextFill(Color.RED);
+		expensesLabel.setText("$54.48");
+		totalLabel.setText("$70.52");
+	}
+	
+	public void changeScene(String path) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+		try {
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("IOException has been caught.");
+		} finally {
+			menuBar.getScene().setRoot(loader.getRoot());
+		}
+	}
+	
+	public void clearTextFields() {
+		
 	}
 	
 }
